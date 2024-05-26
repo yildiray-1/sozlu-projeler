651ecd1e63d157e82a74cffaf68008d1e4e6fb4c
Yıldıray <153164444+yildiray-1@users.noreply.github.com>
2024-05-26 11:27:40 +0300

    Add files via upload

---
+++ egeninki için.py
@@ -0,0 +1,35 @@
+from machine import Pin, I2C
+import ssd1306
+from hcsr04 import HCSR04
+from time import sleep
+
+# ESP32 Pin assignment 
+i2c = I2C(scl=Pin(22), sda=Pin(21))
+sensor = HCSR04(trigger_pin=5, echo_pin=18, echo_timeout_us=10000)
+
+# ESP8266 Pin assignment
+#i2c = I2C(scl=Pin(5), sda=Pin(4))
+#sensor = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=10000)
+
+# LED Pin assignment
+led = Pin(2, Pin.OUT)
+
+oled_width = 128
+oled_height = 64
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+
+while True:
+    oled.fill(0)
+    distance = sensor.distance_cm()
+    print('Distance:', distance, 'cm')
+    oled.text("Distance (cm)", 0, 15)
+    oled.text(str(distance), 0, 35)
+    oled.show()
+
+    # Check distance and control LED
+    if distance < 8:
+        led.on()  # Turn LED on
+    else:
+        led.off()  # Turn LED off
+
+    sleep(1)
